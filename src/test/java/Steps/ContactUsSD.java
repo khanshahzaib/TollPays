771768df18142a25ebcffc6400eacf95a4b46733
khanshahzaib <36466515+khanshahzaib@users.noreply.github.com>
2020-03-24 23:37:42 +0500

    cmt

--- src/test/java/Steps/ContactUsSD.java
+++ src/test/java/Steps/ContactUsSD.java
@@ -1,19 +1,14 @@
 package Steps;
 
 import Base.BaseUtil;
-import DataProvider.ConfigFileReader;
 import Pages.ContactUsPage;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import org.openqa.selenium.support.events.EventFiringWebDriver;
-
-import java.util.concurrent.TimeUnit;
 
 public class ContactUsSD extends BaseUtil {
 
-    ConfigFileReader configFileReader = new ConfigFileReader();
     ContactUsPage contactUsPage;
 
     @Given("user click on the Contact Us button")
@@ -23,14 +18,12 @@ public class ContactUsSD extends BaseUtil {
         contactUsPage.waitForContactUsButton();
         contactUsPage.loadControlsForContactUsButton();
         contactUsPage.contactUsMethod();
-        Thread.sleep(500);
     }
 
     @When("user is on the Contact Us Screen")
     public void userIsOnTheContactUsScreen() throws Throwable {
         System.out.println("user is on the Contact Us Screen");
         contactUsPage = new ContactUsPage(Web_Driver);
-        Thread.sleep(500);
     }
 
     @And("user select the reason of contact")
@@ -40,7 +33,6 @@ public class ContactUsSD extends BaseUtil {
         contactUsPage.waitForSubjectDD();
         contactUsPage.loadControlsForSubjectDD();
         contactUsPage.subjectMethod();
-        Web_Driver.manage().timeouts().implicitlyWait(configFileReader.getImplicitlyWait(), TimeUnit.SECONDS);
     }
 
     @And("user select the account type")
@@ -50,7 +42,6 @@ public class ContactUsSD extends BaseUtil {
         contactUsPage.waitForAccountTypeDD();
         contactUsPage.loadControlsForAccountTypeDD();
         contactUsPage.accountTypeMethod();
-        Thread.sleep(500);
     }
 
     @And("user enter valid email address for contact \"([^\"]*)\"")
@@ -60,7 +51,6 @@ public class ContactUsSD extends BaseUtil {
         contactUsPage.waitForEmailField();
         contactUsPage.loadControlsForEmailField();
         contactUsPage.emailMethod(enterEmailAddress);
-        Thread.sleep(500);
     }
 
     @And("user enter message \"([^\"]*)\"")
@@ -70,7 +60,6 @@ public class ContactUsSD extends BaseUtil {
         contactUsPage.waitForMessageField();
         contactUsPage.loadControlsForMessageField();
         contactUsPage.messageMethod(enterMessage);
-        Thread.sleep(500);
     }
 
     @Then("user click on the Submit button")
@@ -80,6 +69,5 @@ public class ContactUsSD extends BaseUtil {
         contactUsPage.waitForSubmitButton();
         contactUsPage.loadControlsForSubmitButton();
         contactUsPage.submitMethod();
-        Thread.sleep(500);
     }
 }
